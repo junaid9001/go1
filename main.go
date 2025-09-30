62b024947c76dfb74940351c1288527607e9af24
Junaid <mohamedjunaid888@gmail.com>
2025-09-30 11:44:11 +0530

    calculator

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import "fmt"
 
 func oddeven() {
 	var num int
+	fmt.Println("check even or odd")
 	fmt.Println("enter number")
 	fmt.Scanln(&num)
 
@@ -16,6 +17,34 @@ func oddeven() {
 	}
 }
 
+//calculator
+
+func calc() {
+	var num1 int
+	var typ string
+	var num2 int
+    fmt.Println("calculator")
+	fmt.Println("enter first number")
+	fmt.Scanln(&num1)
+	fmt.Println("choose calculation ( +   -   *   /  )")
+	fmt.Scanln(&typ)
+	fmt.Println("choose second number")
+	fmt.Scanln(&num2)
+
+	switch typ {
+	case "+":
+		fmt.Println(num1 + num2)
+	case "-":
+		fmt.Println(num1 - num2)
+	case "*":
+		fmt.Println(num1 * num2)
+	case "/":
+		fmt.Println(num1 / num2)
+	}
+
+}
+
 func main() {
 	oddeven()
+	calc()
 }
